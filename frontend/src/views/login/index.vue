bfa4667c95b0ba8ff70f774e57cf262932b357a9
ReYim_7 <1351494149@qq.com>
2019-06-18 18:07:45 +0800

    实现前端和后端链接

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title"> 学生学籍信息管理系统 </h3>
       </div>
 
       <el-form-item prop="username">
@@ -41,11 +41,11 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登陆</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">初始用户: admin</span>
+        <span> 初始密码: 123456</span>
       </div>
 
     </el-form>
